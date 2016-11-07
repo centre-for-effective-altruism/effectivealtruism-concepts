28d6432f714af5393e5f29483991ec85ca44a89e
colophonemes <dev@colophonemes.com>
2016-11-07 11:24:32 +0000

    Include concepts in the build

--- tasks/metalsmith/content-types/index.js
+++ tasks/metalsmith/content-types/index.js
@@ -2,6 +2,5 @@ module.exports = {
   concept: require('./concept.js'),
   link: require('./link.js'),
   page: require('./page.js'),
-  post: require('./post.js'),
   series: require('./series.js')
 }
